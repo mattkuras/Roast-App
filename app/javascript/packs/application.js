1712c82c83c48ef1fc9bd9c9f49737938527c521
matt_kuras <mattkuras@gmail.com>
2020-12-31 14:51:33 -0500

    add simple_forms, haml, devise for users. configure webpack and add bootstrap and FA

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,17 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+import "@fortawesome/fontawesome-free/css/all"
+require("stylesheets/application.scss")
+
+if (module.hot) {
+    module.hot.accept('./application.js', () => {
+      const NextApp = require('./application.js').default
+      renderComponent(NextApp)
+    })
+  }
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
